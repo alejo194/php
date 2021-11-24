a6cc7e69db2e3049ac983b3b381a6fdf90a0659e
alejo194 <37093748+alejo194@users.noreply.github.com>
2021-11-24 11:47:10 +0800

    Add files via upload

---
+++ jessie/php5.4-apache/conf/test.php
@@ -0,0 +1,14 @@
+<?php  
+header("Content-type: text/html; charset=utf-8");    
+$msdb=mssql_connect("192.168.40.53:2900","sa","maxwin80743314");  
+if (!$msdb) {  
+    echo "connect sqlserver error";  
+    exit;  
+    }  
+mssql_select_db("maxbus",$msdb);  
+$result = mssql_query("SELECT top 5 * FROM logicroute", $msdb);  
+while($row = mssql_fetch_array($result)) {  
+ print_r($row);  
+}  
+mssql_free_result($result);  
+?>
